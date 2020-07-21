4973229f13863a4ad65c0687c13f4bdf1623bab0
sptkl <caobaiyue@gmail.com>
2020-07-21 15:47:59 +0000

    rewrite sql so that we dont have to specify years

--- recipes/sca_e_projections_by_sd/build.sql
+++ recipes/sca_e_projections_by_sd/build.sql
@@ -1,35 +1,27 @@
 CREATE TEMP TABLE tmp as (
     WITH 
     UNPIVOT as (
-        SELECT 
-            t.district,
-            t.projected, 
-            col_name as year, 
-            (CASE WHEN t.projected in 
-                ('PK','K','1','2','3','4','5','6') 
-                THEN replace(col_value, ',', '')::integer 
-            END) as "ps", 
-            (CASE WHEN t.projected in ('7','8') 
-                THEN replace(col_value, ',', '')::integer 
+        select 
+            a._col ->> 'district' as district, 
+            a._col ->> 'projected' as projected,
+            LEFT(b.key, 4) as school_year, 
+            (CASE WHEN a._col ->> 'projected' in 
+                    ('PK','K','1','2','3','4','5','6') 
+                THEN replace(b.value, ',', '')::integer 
+            END) as "ps",
+            (CASE WHEN a._col ->> 'projected' in ('7','8') 
+                THEN replace(b.value, ',', '')::integer 
             END) as "is"
-        from sca_e_projections."2019" t
-        JOIN LATERAL (VALUES
-            ('2018_19', t."2018_19"),
-            ('2019_20', t."2019_20"),
-            ('2020_21', t."2020_21"),
-            ('2021_22', t."2021_22"),
-            ('2022_23', t."2022_23"),
-            ('2024_25', t."2024_25"),
-            ('2025_26', t."2025_26"),
-            ('2026_27', t."2026_27"),
-            ('2027_28', t."2027_28"),
-            ('2028_29', t."2028_29")
-        ) s(col_name, col_value) ON TRUE
+        FROM (
+            select row_to_json(row) as _col 
+            from (select * from sca_e_projections."2019") row) a , 
+            json_each_text(_col) as b
+        where b.key not in ('ogc_fid', 'district', 'projected')
     ),
     MULTIPLY as (
         SELECT
             a.district, 
-            LEFT(a.year,4) as year,
+            a.school_year,
             a.projected,
             a."is"* b.multiplier::numeric as "is",
             a."ps"* b.multiplier::numeric as "ps",
@@ -42,14 +34,15 @@ CREATE TEMP TABLE tmp as (
             '4','5','6','7','8')
     )
     SELECT
-        year as school_year,
+        school_year,
         district,
         subdistrict,
         sum("is")::integer as "is",
         sum("ps")::integer as "ps"
     FROM MULTIPLY
-    GROUP BY district, subdistrict, year
-    ORDER BY district, subdistrict, year
+    GROUP BY district, subdistrict, school_year
+    ORDER BY district, subdistrict, school_year
+
 );
 
 \COPY tmp TO PSTDOUT DELIMITER ',' CSV HEADER;
