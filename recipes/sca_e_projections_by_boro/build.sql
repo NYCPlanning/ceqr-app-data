27afccd43df04195f4cfd74c0bd40a8a5de8705a
sptkl <caobaiyue@gmail.com>
2020-07-22 15:26:34 +0000

    adding documentation

--- recipes/sca_e_projections_by_boro/build.sql
+++ recipes/sca_e_projections_by_boro/build.sql
@@ -1,3 +1,54 @@
+/*
+DESCRIPTION:
+    1. Unpivot sca_e_projections so that we get a table like the following: 
+
+        district |  projected  | year |  hs   
+       ----------+-------------+------+-------
+        ...      | ...         | ...  |   ...
+        1        | PK          | 2026 |   XXX
+        2        | PK          | 2027 |   XXX
+        4        | PK          | 2028 |   XXX
+        ...      | ...         | ...  |   ...
+        2        | K           | 2018 |   XXX
+        1        | K           | 2019 |   XXX
+        3        | K           | 2020 |   XXX
+        ...      | ...         | ...  |   ...
+
+    2. Assign Borough to school districts: 
+        MN: 1 ~ 6
+        BX: 7 ~ 12
+        BK: 13 ~ 23 and 32
+        QN: 24 ~ 30
+        SI: 31
+    
+    3. Aggregate over borough and school year by summing over hs
+
+INPUT:
+    sca_e_projections.:"VERSION" (
+        district character varying,
+        projected character varying,
+        "2018_19" character varying,
+        "2019_20" character varying,
+        "2020_21" character varying,
+        "2021_22" character varying,
+        "2022_23" character varying,
+        "2023_24" character varying,
+        "2024_25" character varying,
+        "2025_26" character varying,
+        "2026_27" character varying,
+        "2027_28" character varying,
+        "2028_29" character varying
+    )
+
+OUTPUT:
+
+    TEMP tmp (
+        year text,
+        borough character varying,
+        hs bigint
+    )
+
+*/
 CREATE TEMP TABLE tmp as (
     WITH 
     unpivot as (
