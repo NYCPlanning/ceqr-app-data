7d6247840f161398f61dfac579f8ee36f9dba05d
mgraber <molly.graber@me.com>
2020-07-22 19:10:33 +0000

    Document dot_traffic_cameras

--- recipes/dot_traffic_cameras/create.sql
+++ recipes/dot_traffic_cameras/create.sql
@@ -1,3 +1,25 @@
+/*
+DESCRIPTION:
+    1. Import nysdec_air_monitoring_stations data to EDM database using PSTDIN
+    2. Create geometry from latitude and longitude
+INPUTS: 
+	PSTDIN >> 
+    TEMP tmp (
+        content text,
+        icon text,
+        id text,
+        latitude double precision,
+        longitude double precision,
+        title text,
+        url text
+)
+OUTPUTS:
+	dot_traffic_cameras.latest(
+                            All fields from TEMP tmp,
+                            geom geometry)
+    )
+*/
+
 CREATE TEMP TABLE tmp (
     content text,
     icon text,
