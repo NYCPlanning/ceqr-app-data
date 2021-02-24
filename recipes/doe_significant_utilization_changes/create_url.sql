c829f429876de6f3a5bf766aafb6d4fa48244c2a
sptkl <caobaiyue@gmail.com>
2021-02-24 17:45:14 +0000

    remove create_url.sql

--- recipes/doe_significant_utilization_changes/create_url.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS doe_pepmeetingurls;
-CREATE TABLE doe_pepmeetingurls (
-    url character varying,
-    school_year character varying,
-    readable_url character varying,
-    date character varying
-);
-
-\COPY doe_pepmeetingurls FROM PSTDIN DELIMITER ',' CSV HEADER;
