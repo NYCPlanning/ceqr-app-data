8b346d8be383ff390c0a85e9da38c09966c9601c
mgraber <mgraber@planning.nyc.gov>
2021-06-25 15:24:41 -0600

    Remove intersection and stretch geog creation

--- recipes/ceqr_school_buildings/create.sql
+++ recipes/ceqr_school_buildings/create.sql
@@ -78,23 +78,8 @@ SELECT
     ie,
     hc,
     he,
-    (CASE 
-        -- Intersections: Create  geometry from x_coord and y_coord
-        WHEN geo_function = 'Intersection'
-        THEN ST_TRANSFORM(ST_SetSRID(ST_MakePoint(geo_x_coord,geo_y_coord),2263),4326)
-        -- Segments: Find the middle of the segment by creating a line from to/from coords
-        WHEN geo_function = 'Stretch'
-        THEN ST_centroid(ST_MakeLine(
-                ST_TRANSFORM(ST_SetSRID(
-                    ST_MakePoint(geo_from_x_coord::NUMERIC, geo_from_y_coord::NUMERIC),2263),4326),
-                ST_TRANSFORM(ST_SetSRID(
-                    ST_MakePoint(geo_to_x_coord::NUMERIC, geo_to_y_coord::NUMERIC),2263),4326)))
-        -- Address points: Create geometry from xy_coord
-        WHEN geo_function IN ('1B','1E') 
-        THEN ST_TRANSFORM(ST_SetSRID(ST_MakePoint(LEFT(geo_xy_coord, 7)::DOUBLE PRECISION,
-                                            RIGHT(geo_xy_coord, 7)::DOUBLE PRECISION),2263),4326)
-        ELSE NULL
-    END)::geometry(Point,4326) as geom
+    (ST_TRANSFORM(ST_SetSRID(ST_MakePoint(LEFT(geo_xy_coord, 7)::DOUBLE PRECISION,
+                                            RIGHT(geo_xy_coord, 7)::DOUBLE PRECISION),2263),4326))::geometry(Point,4326) as geom
 INTO :NAME.:"VERSION"
 FROM tmp;
 
