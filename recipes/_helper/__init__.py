8364bfdf00c7606c43021851a48af15bef108891
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-05-16 11:15:06 -0400

    no CEQR, print secrets

--- recipes/_helper/__init__.py
+++ recipes/_helper/__init__.py
@@ -3,6 +3,8 @@ import os
 from datetime import date
 
 
-CEQR_DATA = create_engine(os.environ["CEQR_DATA"])
+# CEQR_DATA = create_engine(os.environ["CEQR_DATA"])
+print(f"EDM_DATA is f{os.environ['EDM_DATA']}")
+print(f"DO S3 endpoint is f{os.environ['AWS_S3_ENDPOINT']}")
 EDM_DATA = create_engine(os.environ["EDM_DATA"])
 DATE = date.today().strftime("%Y-%m-%d")
