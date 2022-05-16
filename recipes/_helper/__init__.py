959d39735a589f836e1fbc65e67040d3632eb79e
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-05-16 11:27:38 -0400

    echo environmental variables instead

--- recipes/_helper/__init__.py
+++ recipes/_helper/__init__.py
@@ -4,6 +4,5 @@ from datetime import date
 
 
 # CEQR_DATA = create_engine(os.environ["CEQR_DATA"])
-print(f"environmental variables are f{os.environ}")
 EDM_DATA = create_engine(os.environ["EDM_DATA"])
 DATE = date.today().strftime("%Y-%m-%d")
