a9a86b9a85c3ef536d721dadb42e94c28d36f313
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-05-16 11:20:17 -0400

    print all environmental variables

--- recipes/_helper/__init__.py
+++ recipes/_helper/__init__.py
@@ -4,7 +4,6 @@ from datetime import date
 
 
 # CEQR_DATA = create_engine(os.environ["CEQR_DATA"])
-print(f"EDM_DATA is f{os.environ['EDM_DATA']}")
-print(f"DO S3 endpoint is f{os.environ['AWS_S3_ENDPOINT']}")
+print(f"environmental variables are f{os.environ}")
 EDM_DATA = create_engine(os.environ["EDM_DATA"])
 DATE = date.today().strftime("%Y-%m-%d")
