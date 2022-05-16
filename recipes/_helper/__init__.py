995c6e8b1309d8a58e73e94fa051608674573fa1
Sasha Weinstein <70385853+SashaWeinstein@users.noreply.github.com>
2022-05-16 11:09:54 -0400

    switch order of loading CEQR, EDM_DATA environmental variables

--- recipes/_helper/__init__.py
+++ recipes/_helper/__init__.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 import os
 from datetime import date
 
-edm_data_path = os.environ["EDM_DATA"]
-print(f"path loaded from secrets: {edm_data_path}")
-EDM_DATA = create_engine(os.environ["EDM_DATA"])
+
 CEQR_DATA = create_engine(os.environ["CEQR_DATA"])
+EDM_DATA = create_engine(os.environ["EDM_DATA"])
 DATE = date.today().strftime("%Y-%m-%d")
