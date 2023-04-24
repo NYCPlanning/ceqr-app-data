291ea2e10158556bc62ac9606c466dc2964f28b7
Damon McCullough <damonmcc1391@gmail.com>
2023-04-24 15:00:06 -0400

    oops

--- recipes/sca_capacity_projects/runner.sh
+++ recipes/sca_capacity_projects/runner.sh
@@ -15,7 +15,7 @@ VERSION=$DATE
     docker run --rm\
         -v $(pwd)/../:/recipes\
         -w /recipes/$NAME\
-        --env $EDM_DATA
+        # --env $EDM_DATA
         --user $UID\
         nycplanning/docker-geosupport:latest bash -c "python3 build.py" | 
     psql $EDM_DATA --set NAME=$NAME --set VERSION=$VERSION --set ON_ERROR_STOP=1 --file create.sql
